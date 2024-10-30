ae9453e3cacb53fd85bc4afdb454b8b749a543e8
Ayebare Timothy <vexa256@gmail.com>
2024-10-30 12:05:33 +0200

    mail fix

--- app/Http/Controllers/SecondaryScreeningDataCapture.php
+++ app/Http/Controllers/SecondaryScreeningDataCapture.php
@@ -101,7 +101,7 @@ class SecondaryScreeningDataCapture extends Controller
             $recipients = [
                 env('MINISTRY_OF_HEALTH_EMAIL', 'atimothy@ecsahc.org'),
                 'nsekuye.olivier003@gmail.com',
-                'chris@ecsahc.org',
+                'chris@ecsahc.org', 'jessrurangwa@gmail.com', 'sandrine.uwamahoro@rbc.gov.rw', 'ruyangelaurent@gmail.com',
             ];
             $subject = 'Alert: ' . $screeningData['classification'] . ' - POE Screening Notification';
 
